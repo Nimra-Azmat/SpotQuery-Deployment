c55db290bcec37010da1d122e52cbfe3b06053d6
nimra <nimra.azmat@mlsense.ai>
2023-05-04 11:56:33 +0500

    reattempt-3

--- core/settings.py
+++ core/settings.py
@@ -113,7 +113,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend-songs/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
